4287bd50429a0f5695ac244f1fc1b6d2fa4f6637
Perm <wannuwatbuayangyuen@gmail.com>
2024-12-13 16:30:42 +0700

    lap9_1 completed

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
+#include<cmath>
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
 int main(){	
+	double load, per, pay;
+	
 	cout << "Enter initial loan: ";
+	cin >> load;
 	cout << "Enter interest rate per year (%): ";
+	cin >> per;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +26,21 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+
+for(int i = 1; load > 0; i++){
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << i ; 
+	cout << setw(13) << left << load;
+	cout << setw(13) << left << load * per/100.00;
+	cout << setw(13) << left << load + (load * per/100.00);
+	if(pay <= (load + (load * per/100))) cout << setw(13) << left << pay;
+    else{ 
+		pay = load + (load * per/100.00);
+        cout << setw(13) << left << pay;
+	}
+	cout << setw(13) << left << (load + (load * per/100.00)) - pay;
+	load = (load +(load * per/100.00)) - pay;
 	cout << "\n";	
-	
+}
 	return 0;
 }
